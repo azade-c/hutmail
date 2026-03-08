d998890ec8c769c63e96964fcc7a0b9a3d2d70e4
Huttix 🦫 <azade+huttix@hey.com>
2026-03-08 22:47:32 +0000

    test: stabilize system smoke assertion

--- test/system/smoke_test.rb
+++ test/system/smoke_test.rb
@@ -3,6 +3,10 @@ require "application_system_test_case"
 class SmokeTest < ApplicationSystemTestCase
   test "loads sign in page" do
     visit new_session_path
-    assert_text "Sign in"
+
+    assert_current_path new_session_path
+    assert_field "email_address"
+    assert_field "password"
+    assert_link "Forgot password?"
   end
 end
