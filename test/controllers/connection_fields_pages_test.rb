aeb08e7fa114e3a4af2ff9b0ca059020dd5e42eb
François Catuhe <francois@hey.com>
2026-03-11 19:53:56 +0100

    fix: use nested route helpers in all vessel-scoped views
    Fix remaining non-nested route helpers in:
    - mail_accounts/index.html.erb (new, show links)
    - mail_accounts/show.html.erb (edit, delete links)
    - bundles/index.html.erb (show link)
    
    Expand regression tests to cover index, show, create,
    update, and error rendering for all affected pages.

--- test/controllers/connection_fields_pages_test.rb
+++ test/controllers/connection_fields_pages_test.rb
@@ -18,8 +18,54 @@ class ConnectionFieldsPagesTest < ActionDispatch::IntegrationTest
   end
 
   test "edit mail account page renders" do
-    mail_account = mail_accounts(:gmail)
-    get edit_vessel_mail_account_path(@vessel, mail_account)
+    get edit_vessel_mail_account_path(@vessel, mail_accounts(:gmail))
+    assert_response :success
+  end
+
+  test "create mail account succeeds" do
+    assert_difference "MailAccount.count", 1 do
+      post vessel_mail_accounts_path(@vessel), params: {
+        mail_account: {
+          name: "Proton", short_code: "PR",
+          imap_server: "imap.proton.me", imap_port: 993, imap_encryption: "ssl",
+          imap_username: "test@proton.me", imap_password: "secret",
+          smtp_server: "smtp.proton.me", smtp_port: 465, smtp_encryption: "ssl",
+          smtp_username: "test@proton.me", smtp_password: "secret"
+        }
+      }
+    end
+    assert_redirected_to vessel_mail_account_path(@vessel, MailAccount.last)
+  end
+
+  test "create mail account renders errors on invalid params" do
+    assert_no_difference "MailAccount.count" do
+      post vessel_mail_accounts_path(@vessel), params: {
+        mail_account: { name: "", short_code: "" }
+      }
+    end
+    assert_response :unprocessable_entity
+  end
+
+  test "update settings succeeds" do
+    patch vessel_settings_path(@vessel), params: {
+      vessel: { name: "Renamed Boat" }
+    }
+    assert_redirected_to edit_vessel_settings_path(@vessel)
+    assert_equal "Renamed Boat", @vessel.reload.name
+  end
+
+  test "show mail account page renders" do
+    get vessel_mail_account_path(@vessel, mail_accounts(:gmail))
+    assert_response :success
+  end
+
+  test "mail accounts index page renders" do
+    get vessel_mail_accounts_path(@vessel)
+    assert_response :success
+  end
+
+  test "bundles index page renders" do
+    get vessel_bundles_path(@vessel)
     assert_response :success
   end
 end
