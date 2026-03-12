20ca2cee6bde6117d48436d2ad8c522455aa6190
Huttix 🦫 <azade+huttix@hey.com>
2026-03-12 13:26:04 +0000

    feat: persist discovered SMTP auth method to avoid retrying

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -8,13 +8,20 @@ class ApplicationMailer < ActionMailer::Base
   SMTP_AUTH_METHODS = %i[plain login].freeze
 
   def deliver_with_auth_fallback(account, &block)
-    SMTP_AUTH_METHODS.each_with_index do |auth_method, index|
+    methods_to_try = if account.smtp_auth_method.present?
+      [ account.smtp_auth_method.to_sym ]
+    else
+      SMTP_AUTH_METHODS
+    end
+
+    methods_to_try.each_with_index do |auth_method, index|
       message = block.call(auth_method)
       message.deliver_now
+      account.update_column(:smtp_auth_method, auth_method.to_s) if account.smtp_auth_method.blank?
       return message
     rescue Net::SMTPSyntaxError, Net::SMTPFatalError => e
       raise unless e.message.include?("mechanism") || e.message.include?("auth")
-      raise if index == SMTP_AUTH_METHODS.size - 1
+      raise if index == methods_to_try.size - 1
     end
   end
 
